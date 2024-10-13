333cd0051b4e5e94a3b3c4efbaa1d3704afd1ca8
kolya343434 <147270821+kolya343434@users.noreply.github.com>
2024-10-13 16:30:21 +0300

    local ru

--- laba1/main.cpp
+++ laba1/main.cpp
@@ -1,10 +1,11 @@
 
 #include "interface.h"
-
+#include <locale>  // Для работы с локалями
+#include <clocale> // Для setlocale
 
 int main() {
 
-    
+    std::setlocale(LC_ALL, "ru_RU.UTF-8");
     interface();
     
     
